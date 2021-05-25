98cd0b836641c81d77b73e03e6a64034d1f8db37
Dani <>
2021-05-25 10:20:42 +0000

    fixed a private-message issue

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ client.on('ready', () => {
 
 	privateMessage(client, 'caca', 'vas a quedar baniado por desir caca')
 
-	client.users.fetch('644798160291627028').then((user) => {
-		user.send('hola')
-	})
 
 	command(client, 'caca', message => {
 		message.channel.send('cacota')
